2ce03ec00c9e58df7d29ee056a26965c2277afd1
shockham <samuel.hockham@gmail.com>
2019-07-06 14:30:19 +0100

    Update to use infuse 0.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
-use std::rc::Rc;
 
-use infuse::{RenderItem, Renderer};
+use infuse::{request_animation_frame, RenderItem, Renderer};
 use instant;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -34,28 +32,14 @@ pub fn start() -> Result<(), JsValue> {
 
     let mut render_items = vec![render_item];
 
-    let f = Rc::new(RefCell::new(None));
-    let g = f.clone();
-
-    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+    request_animation_frame!({
         let tick_time = instant::now();
         render_items[0].set_uniform(
             "time".to_string(),
             ((tick_time / 500f64) as f32, 0f32, 0f32, 0f32),
         );
         renderer.draw(&render_items).unwrap();
-
-        request_animation_frame(f.borrow().as_ref().unwrap());
-    }) as Box<dyn FnMut()>));
-
-    request_animation_frame(g.borrow().as_ref().unwrap());
+    });
 
     Ok(())
-}
-
-fn request_animation_frame(f: &Closure<dyn FnMut()>) {
-    web_sys::window()
-        .expect("no global `window` exists")
-        .request_animation_frame(f.as_ref().unchecked_ref())
-        .expect("should register `requestAnimationFrame` OK");
 }
